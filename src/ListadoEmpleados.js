b4f3cd0bb89c77d757c3ef47ff7373cb09cb0141
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-12-10 20:29:53 -0600

    Listado de empleados

--- src/ListadoEmpleados.js
+++ src/ListadoEmpleados.js
@@ -4,10 +4,36 @@ import axios from 'axios';
 const ListadoEmpleados = () => {
     const [empleados, setEmpleados] = useState([]);
 
-    useEffect(()) => {
+    useEffect(() => {
         const fecthEmpleados = async () => {
             const response = await axios.get('https://674c84c054e1fca9290cd05f.mockapi.io/api/examen/empleado');
       setEmpleados(response.data);
     };
     fetchEmpleados();
   }, []);
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>DNI</th>
+          <th>Dirección</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {empleados.map((empleado) => (
+          <tr key={empleado.id}>
+            <td>{empleado.nombre}</td>
+            <td>{empleado.dni}</td>
+            <td>{empleado.direccion}</td>
+            <td>{empleado.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ListadoEmpleados;
